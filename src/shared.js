7e356319439bb38d9b45ce3aaeabba47c07bc541
Marcio Woitek <mwoitekjr@gmail.com>
2023-06-15 17:26:19 -0300

    Display/hide side bar

--- src/shared.js
+++ src/shared.js
@@ -1,19 +1,30 @@
 const backdrop = document.querySelector('.backdrop');
+const mobileNav = document.querySelector('.mobile-nav');
 const modal = document.querySelector('.modal');
 const noButton = document.querySelector('.modal__action--negative');
 const selectPlanButtons = document.querySelectorAll('.plan button');
+const toggleButton = document.querySelector('.toggle-button');
 
 selectPlanButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    modal.style.display = 'block';
-    backdrop.style.display = 'block';
+    modal.classList.add('open');
+    backdrop.classList.add('open');
   });
 });
 
 const hideModal = () => {
-  modal.style.display = 'none';
-  backdrop.style.display = 'none';
+  modal.classList.remove('open');
+  backdrop.classList.remove('open');
 };
 
-backdrop.addEventListener('click', hideModal);
+backdrop.addEventListener('click', () => {
+  mobileNav.classList.remove('open');
+  hideModal();
+});
+
 noButton.addEventListener('click', hideModal);
+
+toggleButton.addEventListener('click', () => {
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
+});
